87eca3d25c2ac6f5333aef1e6c2e6aa5e20b1f65
Wendel <wendel824829has@gmail.com>
2024-07-31 12:45:57 -0300

    atualizando navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import { useState } from "preact/hooks"
-import '../styles/Navbar.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { useNavigate } from 'react-router-dom';
 
-export default function Navbar({children}){
+function BasicExample() {
 
-    return(
-        <>
-        <div className="Nav-Box">
+    const navigate = useNavigate();
 
-        </div>
-        </>
-    )
+    const navigatetoHome = () => navigate('/home');
+    const navigatetoCalendar = () => navigate('/calendario');
+    const navigatetoAgendar = () => navigate('/agendar')
+
+    return (
+    <Navbar expand="lg" className="Navbar">
+    <Container>
+        <Navbar.Brand onClick={navigatetoHome}>SESI ARDS</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+            <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+            <Dropdown.Menu>
+                <Dropdown.Item id="homeitem" onClick={navigatetoHome}>Home</Dropdown.Item>
+                <Dropdown.Item id='agendaritem' onClick={navigatetoAgendar}>Agendar</Dropdown.Item>
+                <Dropdown.Item id='calendarioitem' onClick={navigatetoCalendar}>Calendário</Dropdown.Item>
+            </Dropdown.Menu>
+            </Dropdown.Toggle>
+            </Dropdown>
+
+            
+        </Nav>
+        </Navbar.Collapse>
+    </Container>
+    </Navbar>
+);
 }
 
-const MenuModal = ({modal, })=>{
-    if(modal===true)
-    return(  
-        <>  
-     <div>
-        Este é um Modal
-        <button onClick={() => setModal(false)}>Fechar</button>
-    </div>
-    </>
-    )
-    
-    return null
-}
+export default BasicExample;
