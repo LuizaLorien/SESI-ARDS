cc87daa5f02fd6ac45c8f4743adfc6efc4e6bd92
Wendel <wendel824829has@gmail.com>
2024-08-02 12:04:47 -0300

    arrumando botao

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,32 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Dropdown from 'react-bootstrap/Dropdown';
+import '../styles/Navbar.css'
+import menuicon from '../assets/calendar4-week.svg'
 
 function BasicExample() {
 
     return (
-    <Navbar expand="lg" className="Navbar">
+    <Navbar fixed='top' expand="lg" className="Navbar">
     <Container>
-        <Navbar.Brand >SESI ARDS</Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-            <Dropdown>
+            <Dropdown img= {menuicon}>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
-            <Dropdown.Menu>
-                <Dropdown.Item href='/home' id="homeitem" >Home</Dropdown.Item>
-                <Dropdown.Item href='/agendar' id='agendaritem' >Agendar</Dropdown.Item>
-                <Dropdown.Item href='calendario' id='calendarioitem' >Calendário</Dropdown.Item>
+            <Dropdown.Menu id='dropdown'>
+                <Dropdown.Item href='/home' id="homeitem">Home</Dropdown.Item>
+                <Dropdown.Item href='/agendar' id='agendaritem'>Agendar</Dropdown.Item>
+                <Dropdown.Item href='calendario' id='calendarioitem'>Calendário</Dropdown.Item>
             </Dropdown.Menu>
             </Dropdown.Toggle>
             </Dropdown>
-
+            <Navbar.Brand >SESI ARDS</Navbar.Brand>
             
+            <div className='profile'>
+            </div>
         </Nav>
         </Navbar.Collapse>
+        
     </Container>
     </Navbar>
 );
